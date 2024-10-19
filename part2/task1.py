a85337c03311a7c4ac50b09cb808ddd31df9fcc4
Andrey <127301400+progito@users.noreply.github.com>
2024-10-19 03:16:05 -0700

    Update task1.py

---
+++ part2/task1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Create a NumPy array
+array = np.array([1, 2, 3, 4, 5])
+
+# Perform a basic operation
+squared_array = array ** 2
+
+print(squared_array)  # Output: [ 1  4  9 16 25]
